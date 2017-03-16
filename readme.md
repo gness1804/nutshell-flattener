f1175e4056d103e6f482eb01e9164956744ccb06
gness1804 <graham.nessler@gmail.com>
2017-03-16 16:49:11 -0600

    Reorganize tests into a more logical structure

--- readme.md
+++ readme.md
@@ -6,4 +6,4 @@ Testing:
 
 * To run the tests, clone down this repo, `npm install`, and then `npm run test` or `mocha`.
 * The tests will run, and the results will appear in the terminal. If the install was correct and no code is altered from the repo, all four tests will pass.
-* The tests cover several input cases to ensure that the function works. 
+* The tests cover several input cases to ensure that the function works.
--- test/main.js
+++ test/main.js
@@ -3,15 +3,24 @@ const assert = require('chai').assert
 const flattener = require('../scripts')
 
 describe('flattener', function () {
-  it('should return a flat array', function () {
+  it('should return a flat array when the input is a flat array', function () {
     const simple = [1, 2, 3, 4]
-    const moreComplex = [[1,2,[3]],4]
-    const stillMoreComplex = [[1,[[2]],[3]],4]
-    const wayMoreComplex = [[1, [2, [3]]], [4, [5, [6]]]]
-
     assert.deepEqual(flattener(simple), [ 1, 2, 3, 4 ], 'The simplest case.')
+  });
+
+  it('should return a flat array when the input is a more complex non-flat array', function () {
+    const moreComplex = [[1,2,[3]],4]
     assert.deepEqual(flattener(moreComplex), [1, 2, 3, 4], 'The more complex case.')
+
+  });
+
+  it('should return a flat array when the input is a still more complex non-flat array', function () {
+    const stillMoreComplex = [[1,[[2]],[3]],4]
     assert.deepEqual(flattener(stillMoreComplex), [1, 2, 3, 4], 'The still more complex case.')
+  });
+
+  it('should return a flat array when the input is a way more complex non-flat array', function () {
+    const wayMoreComplex = [[1, [2, [3]]], [4, [5, [6]]]]
     assert.deepEqual(flattener(wayMoreComplex), [1, 2, 3, 4, 5, 6], 'The way more complex case.')
   });
 });
